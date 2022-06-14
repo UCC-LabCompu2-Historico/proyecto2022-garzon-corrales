6606c8cb6d094f2bc965c8f8ebb1653a5d44d0b0
Antonio <2100542@ucc.edu.ar>
2022-06-14 17:32:33 -0300

    .

--- funciones.js
+++ funciones.js
@@ -1,3 +1,217 @@
+
+function choqueRectangular(obj1,obj2){
+    return obj1.pos.x + obj1.width/2 >= obj2.pos.x - obj2.width/2 &&
+        obj1.pos.x - obj1.width/2 <= obj2.width/2 + obj2.pos.x &&
+        obj1.pos.y + obj1.height/2 >= obj2.pos.y - obj2.height/2 &&
+        obj1.pos.y - obj1.height/2 <= obj2.height/2 + obj2.pos.y;
+}
+
+let Controller = function(){
+    this.up = false;
+    this.down = false;
+    this.left = false;
+    this.right = false;
+    //adding client test
+
+    window.addEventListener('keydown', function(e){
+        switch(e.code){
+            case "ArrowUp":
+            case "KeyW":
+                this.up = true;
+                break;
+            case "ArrowLeft":
+            case "KeyA":
+                this.left = true;
+                break;
+            case "ArrowDown":
+            case "KeyS":
+                this.down = true;
+                break;
+            case "ArrowRight":
+            case "KeyD":
+                this.right = true;
+                break;
+            case "Space":
+        }
+    }.bind(this))
+
+    window.addEventListener('keyup', function(e){
+        switch(e.code){
+            case "ArrowUp":
+            case "KeyW":
+                this.up = false;
+                break;
+            case "ArrowLeft":
+            case "KeyA":
+                this.left = false;
+                break;
+            case "ArrowDown":
+            case "KeyS":
+                this.down = false;
+                break;
+            case "ArrowRight":
+            case "KeyD":
+                this.right = false;
+                break;
+            case "Space":
+                this.jump = false;
+                break;
+            case "Ctrl":
+            case "KeyX":
+        }
+    }.bind(this))
+}
+
+
+
+let auto= {
+    height:240,
+    width:100,
+    pos:{x:270,y:465},
+    imagen: new Image(),
+    controller: new Controller(),
+    actualizar: function () {
+        var a=1;
+        if (this.controller.up && (this.pos.y!=-30)) {
+            this.pos.y -= a;
+        } else if (this.controller.down && (this.pos.y!=500)) {
+            this.pos.y += a;
+        }
+
+        if (this.controller.right && (this.pos.x!=540)) {
+            this.pos.x += a;
+        } else if (this.controller.left && (this.pos.x!=-30)) {
+            this.pos.x -= a;
+        }
+
+    },
+
+    dibujar : function () {
+        var canvas = document.getElementById(elementid = "canvas");
+        var ctx = canvas.getContext("2d");
+        this.imagen.src = "imagenes/auto13.png";
+        ctx.drawImage(this.imagen, this.pos.x,this.pos.y);
+
+    },
+}
+
+let obstaculo1={
+    height:230,
+    width:100,
+    pos:{x:50,y:-269},
+    vehiculo: new Image(),
+
+
+    dibujar: function (){
+        var canvas = document.getElementById(elementid = "canvas");
+        var ctx = canvas.getContext("2d");
+        this.vehiculo.src="imagenes/policia1.png";
+        ctx.drawImage(this.vehiculo, this.pos.x,this.pos.y);
+        var al;
+        al=Math.floor(Math.random()*650);
+
+
+        this.pos.y+=1;
+        if(this.pos.y>800){
+
+            if(al>250){
+                this.pos.y=-al;
+            }
+            else{
+                this.pos.y=-678;
+            }
+        }
+    },
+
+}
+
+
+
+let obstaculo2={
+    height:230,
+    width:100,
+    pos:{x:270,y:-841},
+    vehiculo: new Image(),
+
+
+
+    dibujar: function (){
+        var canvas = document.getElementById(elementid = "canvas");
+        var ctx = canvas.getContext("2d");
+        this.vehiculo.src="imagenes/camioneta1.png";
+        ctx.drawImage(this.vehiculo, this.pos.x,this.pos.y);
+        var al;
+        al=Math.floor(Math.random()*650);
+
+
+        this.pos.y+=1;
+        if(this.pos.y>800){
+            if(al>250){
+                this.pos.y=-al;
+            }
+            else{
+                this.pos.y=-471;
+            }
+        }
+    },
+
+}
+let obstaculo3={
+    height:230,
+    width:100,
+    pos:{x:475,y:-1350},
+    vehiculo: new Image(),
+
+
+    dibujar: function (){
+        var canvas = document.getElementById(elementid = "canvas");
+        var ctx = canvas.getContext("2d");
+        this.vehiculo.src="imagenes/auto23.png";
+        ctx.drawImage(this.vehiculo, this.pos.x,this.pos.y);
+        var al;
+        al=Math.floor(Math.random()*650);
+
+
+        this.pos.y+=1;
+        if(this.pos.y>800){
+            if(al>250){
+                this.pos.y=-al;
+            }
+            else{
+                this.pos.y=-978;
+            }
+        }
+    },
+
+}
+
+
+
+
+function inicio(){
+    var canvas = document.getElementById(elementid = "canvas");
+
+    canvas.width=canvas.width;
+    auto.actualizar();
+    auto.dibujar();
+    obstaculo1.dibujar();
+    obstaculo2.dibujar();
+    obstaculo3.dibujar();
+    if(choqueRectangular(auto, obstaculo1) || choqueRectangular(auto,obstaculo2) || choqueRectangular(auto,obstaculo3)){
+        window.open("game_over.html", "_self");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
 /**
  * Se envia el formulario obtenido para utilizar la informacion (nombre del jugador, auto seleccionado).
  * @method pasarFormulario
